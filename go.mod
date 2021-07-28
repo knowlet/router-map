99341c0fd10bd2517effee7ebc460a0c9765c9bf
Albert Hsieh <knowlet@users.noreply.github.com>
2021-07-28 23:50:26 +0000

    feat: add geoip2

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/go-playground/validator/v10 v10.7.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/ip2location/ip2location-go v8.3.0+incompatible
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
---
+++ pkg/geoip2/geoip2.go
@@ -0,0 +1,48 @@
+package geoip2
+
+import (
+	"fmt"
+
+	"github.com/ip2location/ip2location-go"
+)
+
+func Getip(ip string) (string, string, float32, float32, error) {
+	db, err := ip2location.OpenDB("../IP2LOCATION-LITE-DB5.BIN")
+
+	if err != nil {
+		return "", "", 0, 0, err
+	}
+
+	results, err := db.Get_all(ip)
+
+	if err != nil {
+		return "", "", 0, 0, err
+	}
+
+	fmt.Printf("country_short: %s\n", results.Country_short)
+	fmt.Printf("country_long: %s\n", results.Country_long)
+	fmt.Printf("region: %s\n", results.Region)
+	fmt.Printf("city: %s\n", results.City)
+	// fmt.Printf("isp: %s\n", results.Isp)
+	fmt.Printf("latitude: %f\n", results.Latitude)
+	fmt.Printf("longitude: %f\n", results.Longitude)
+	// fmt.Printf("domain: %s\n", results.Domain)
+	// fmt.Printf("zipcode: %s\n", results.Zipcode)
+	// fmt.Printf("timezone: %s\n", results.Timezone)
+	// fmt.Printf("netspeed: %s\n", results.Netspeed)
+	// fmt.Printf("iddcode: %s\n", results.Iddcode)
+	// fmt.Printf("areacode: %s\n", results.Areacode)
+	// fmt.Printf("weatherstationcode: %s\n", results.Weatherstationcode)
+	// fmt.Printf("weatherstationname: %s\n", results.Weatherstationname)
+	// fmt.Printf("mcc: %s\n", results.Mcc)
+	// fmt.Printf("mnc: %s\n", results.Mnc)
+	// fmt.Printf("mobilebrand: %s\n", results.Mobilebrand)
+	// fmt.Printf("elevation: %f\n", results.Elevation)
+	// fmt.Printf("usagetype: %s\n", results.Usagetype)
+	// fmt.Printf("addresstype: %s\n", results.Addresstype)
+	// fmt.Printf("category: %s\n", results.Category)
+	fmt.Printf("api version: %s\n", ip2location.Api_version())
+
+	db.Close()
+	return results.Region, results.City, results.Latitude, results.Longitude, nil
+}
